db8b81ea527a95e4b0c52457da41e2fd8b3c9bfc
LOHITH <35166080+lohith9@users.noreply.github.com>
2018-07-08 11:49:58 +0530

    Create PIR - Pyramids.cpp

---
+++ PIR - Pyramids.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {     // waste problem only formula
+   ios::sync_with_stdio(false);
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      double u,v,w,U,V,W,vol,a,b=12;
+      cin>>u>>v>>w>>W>>V>>U;
+      
+        /*steps to calculate volume of a 
+        Tetrahedron using formula*/
+        a=4*(pow(u,2)*pow(v,2)*pow(w,2)) 
+           - pow(u,2)*pow((pow(v,2)+pow(w,2)-pow(U,2)),2) 
+           - pow(v,2)*pow((pow(w,2)+pow(u,2)-pow(V,2)),2) 
+           - pow(w,2)*pow((pow(u,2)+pow(v,2)-pow(W,2)),2) 
+           + (
+              pow(v,2)+pow(w,2)-pow(U,2))*
+              (pow(w,2)+pow(u,2)-pow(V,2))*
+              (pow(u,2)+pow(v,2)-pow(W,2)
+             );
+        vol = sqrt(a);
+        vol /= b;
+      
+      cout <<fixed <<setprecision(4) << vol<<"\n";
+   }   
+   return 0;
+}
